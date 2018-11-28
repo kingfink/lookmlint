a72b4d526fb0c38b7e2f8745f2a190c06712fc02
Ryan Tuck <ryntck@gmail.com>
2018-11-28 11:36:49 -0600

    flesh out setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup, find_packages
 
+with open('readme.md') as f:
+    long_description = f.read()
+
 setup(
     name='lookmlint',
-    version='0.1',
+    description='Linter for LookML',
+    version='0.1.0',
+    author='Ryan Tuck',
+    author_email='ryan.tuck@warbyparker.com',
+    url='https://github.com/WarbyParker/lookmlint',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'attrs',
         'click',
         'pyyaml',
     ],
-    packages=find_packages(),
+    packages=['lookmlint'],
     entry_points={
         'console_scripts': [
             'lookmlint = lookmlint.cli:cli',
